268042704472d64cd1db2f4e0999321eec20449e
cpu1001 <cpu.cpu@hotmail.com>
2018-05-06 18:02:08 +0200

    hash user pass before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,20 +1,33 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 
+ var password = 'abc1233';
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     })
+// });
 
 
+var hashedPassword = '$2a$10$U8R/rpMrWZ.knQhRLc1j9Onu13CA8QudJ5b5QNPAyfcK7HCFG4PRu';
 
-var data = {
-    id:10
-};
+bcrypt.compare(password, hashedPassword, (err, res)=>{
+    console.log(res);
+});
 
-var token = jwt.sign (data, '123abc');
 
-console.log(token);
+// var data = {
+//     id:10
+// };
 
-var decoded = jwt.decode(token, '123abc');
-console.log('Decoded var: ' + decoded);
+// var token = jwt.sign (data, '123abc');
+
+// console.log(token);
+
+// var decoded = jwt.decode(token, '123abc');
+// console.log('Decoded var: ' + decoded);
 
 
 
